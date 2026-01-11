9e400676140ee55d4f66229e4ba67d6dddd77b6f
CLAUDIO ROBERTO DE CARVALHO <claudfatec@gmail.com>
2026-01-11 20:45:57 -0300

    Finalizada estrutura de pastas e base de dados inicial em #1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#comentario para teste no trello
+import json
+
+def carregar_dados():
+    with open('data/biblia.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def calcular_plano_detalhado():
+    livros = carregar_dados()
+    total_capitulos = sum(l['capitulos'] for l in livros)
+    media_diaria = total_capitulos / 365
+    
+    print(f"Total de capítulos carregados: {total_capitulos}")
+    print(f"Meta diária aproximada: {media_diaria:.2f} capítulos")
+    
+    # Aqui entrará a lógica para distribuir os nomes dos livros
+    # que faremos na próxima Sprint.
+
+if __name__ == "__main__":
+    calcular_plano_detalhado()
