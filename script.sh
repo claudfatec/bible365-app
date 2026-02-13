474f6ab925b719d38b625c7e3a10d1362291ac77
CLAUDIO ROBERTO DE CARVALHO <claudfatec@gmail.com>
2026-02-12 23:20:23 -0300

    Configuração de CI/CD com GitHub Actions

--- script.sh
+++ script.sh
@@ -1,1 +1,5 @@
 streamlit run app.py
+
+https://web.whatsapp.com/send/?1=pt_BR&phone=5511910413322&text=Ol%C3%A1+%F0%9F%98%89%2C+meu+c%C3%B3digo+de+oferta+%C3%A9+%406T91+%28n%C3%A3o+apague+essa+mensagem%29
+https://web.whatsapp.com/send/?1=pt_BR&phone=551158247335&text=Ol%C3%A1+%F0%9F%98%89%2C+gostaria+de+conhecer+os+planos+de+Internet
+https://web.whatsapp.com/send/?1=pt_BR&phone=551146534446&text=Ol%C3%A1+%F0%9F%98%89%2C+gostaria+de+conhecer+os+planos+de+Internet
